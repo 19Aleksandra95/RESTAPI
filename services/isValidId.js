811c0663ba77bc711f5464beb4639bd290187880
19Aleksandra95 <polar.heder@gmail.com>
2024-01-16 11:56:12 +0100

    fixed valid

--- services/isValidId.js
+++ services/isValidId.js
@@ -2,8 +2,10 @@ const {isValidObjectId} = require("mongoose");
 
 const isValidId = (req, res, next) => {
     const {id} = req.params;
-    if(isValidObjectId(id)) {
-        res.status(400).json({"message": `${id} is not valid id`});
+    if(!isValidObjectId(id)) {
+
+    res.status(400).json(`message:${id} is not valid id`) ;
+
     }
     next();
 }
