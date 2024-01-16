db2e2158f25522336f833feb2ed949f046687d20
19Aleksandra95 <polar.heder@gmail.com>
2024-01-16 11:10:02 +0100

    delete id

--- models/contact.js
+++ models/contact.js
@@ -2,12 +2,6 @@ const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema(
   {
-    _id: {
-      type: String,
-      required: true,
-      match: (/^[0-9a-fA-F]{24}$/),
-    },
-
     name: {
       type: String,
       required: [true, "Set name for contact"],
